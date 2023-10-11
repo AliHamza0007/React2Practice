aaf0fdbd98e639734358a891764487c90142e1cf
AliHamza0007 <141808379+AliHamza0007@users.noreply.github.com>
2023-10-11 23:17:12 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Clock from "./Pages/Clock";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Home from "./Pages/Home";
 import Fetch from "./pages/Fetch";
+import Clock from "./Pages/Clock";
 const App = () => {
   return (
     <BrowserRouter>
